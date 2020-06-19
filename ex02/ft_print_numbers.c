ad0dd91a673632d4a5533ba8413f1b2c23cd1979
Tiro Mosoeu <tmosoeu@c4r7s10.wethinkcode.co.za>
2020-06-19 10:12:38 +0200

    ex02 is done

---
+++ ex02/ft_print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <unistd.h>
+
+void ft_putchar(char c) {
+	write (STDOUT_FILENO, &c, 1);
+}
+
+void ft_print_numbers(void) {
+	char digits;
+	digits = '0';
+	while (digits <= '9') {
+		ft_putchar(digits);
+		digits++;
+	}
+}
+
+int main(void) {
+	ft_print_numbers();
+		return 0;
+}
+
