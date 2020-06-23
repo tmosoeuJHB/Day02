9aa5b98087c81abed8dbb1a1aa7f0be80253fe70
Tiro Mosoeu <tmosoeu@c4r7s10.wethinkcode.co.za>
2020-06-23 11:41:28 +0200

    ex03 is done

--- ex03/ft_is_negative.c
+++ ex03/ft_is_negative.c
@@ -1,21 +1,20 @@
-#include <stdio.h>
-#include <unistd.h>
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_is_negative.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tmosoeu <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/06/23 11:40:36 by tmosoeu           #+#    #+#             */
+/*   Updated: 2020/06/23 11:41:07 by tmosoeu          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 
-void ft_putchar(char c) {
-
-	write (1, &c, 1);
-}
 void ft_is_negative(int n) {
 	if (n >= 0) {
 		ft_putchar('P');
 	}
 	else { ft_putchar('N');
 	}
-}
-int main(void){
-	ft_is_negative( 252);
-	ft_is_negative( -252);
 
-	return 0;
-}
